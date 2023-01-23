58a6f979f807043b85655bec540a39ebbcd8ce1f
NGUYEN Hong San <hongsan@gmail.com>
2023-01-23 23:08:13 +0700

    value object with builder

--- examples/customer/model/street_address.go
+++ examples/customer/model/street_address.go
@@ -1,5 +1,7 @@
 package model
 
+import scyna "github.com/scyna/core"
+
 type StreetAddress struct {
 	street   string
 	district string
@@ -21,4 +23,45 @@ func (a StreetAddress) Province() string {
 
 func (a StreetAddress) Country() string {
 	return a.country
+}
+
+type streetAddressBuilder struct {
+	street   string
+	district string
+	province string
+	country  string
+}
+
+func NewStreetAddressBuilder() *streetAddressBuilder {
+	return &streetAddressBuilder{}
+}
+
+func (b *streetAddressBuilder) SetStreet(street string) *streetAddressBuilder {
+	b.street = street
+	return b
+}
+
+func (b *streetAddressBuilder) SetDistrict(district string) *streetAddressBuilder {
+	b.district = district
+	return b
+}
+
+func (b *streetAddressBuilder) SetProvive(province string) *streetAddressBuilder {
+	b.province = province
+	return b
+}
+
+func (b *streetAddressBuilder) SetCountry(country string) *streetAddressBuilder {
+	b.country = country
+	return b
+}
+
+func (b *streetAddressBuilder) Build() (StreetAddress, scyna.Error) {
+	/*TODO validate*/
+	return StreetAddress{
+		street:   b.street,
+		district: b.district,
+		province: b.province,
+		country:  b.country,
+	}, nil
 }
