e7fbec0d0df91cb4169a6aa2258aed402fee5a7c
KhanhND <khanhnd162@gmail.com>
2023-01-27 12:06:26 +0700

    update es

--- examples/chat/account/build/main.go
+++ examples/chat/account/build/main.go
@@ -11,13 +11,15 @@ const MODULE_CODE = "chat_account"
 
 func main() {
 	scyna.RemoteInit(scyna.RemoteConfig{
-		ManagerUrl: "https://localhost:8081",
+		ManagerUrl: "http://localhost:8081",
 		Name:       MODULE_CODE,
 		Secret:     "123456789aA@#",
 	})
 	defer scyna.Release()
 
 	domain.AttachRepositoryCreator(repository.NewRepository)
+
+	scyna.InitEventStore("chat_account")
 
 	scyna.RegisterCommand("/scyna.example/user/create", account.CreateAccountHandler)
 	scyna.Start()
