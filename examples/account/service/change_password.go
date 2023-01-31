63eb920b8dc467910eeae10327e8a6ef8e645043
NGUYEN Hong San <hongsan@gmail.com>
2023-01-31 11:05:44 +0700

    update change password

--- examples/account/service/change_password.go
+++ examples/account/service/change_password.go
@@ -23,17 +23,21 @@ func ChangePasswordHandler(ctx *scyna.Endpoint, request *proto.ChangePasswordReq
 		return ret
 	}
 
-	// if ret = repository.UpdatePassword(ctx, account); ret != nil {
-	// 	return ret
-	// }
+	command := scyna.NewCommand(&ctx.Context).
+		SetAggregateID(account.ID).
+		//SetChannel(PASSWORD_CHANGED_CHANNEL).
+		SetEvent(&proto.PasswordChanged{
+			Id:      account.ID,
+			Current: request.Current,
+			Future:  request.Future})
 
-	// if ret = ctx.StoreEvent(account.ID, PASSWORD_CHANGED_CHANNEL,
-	// 	&proto.PasswordChanged{
-	// 		Id:      account.ID,
-	// 		Current: request.Current,
-	// 		Future:  request.Future}); ret != nil {
-	// 	return ret
-	// }
+	if ret = repository.UpdatePassword(command, account); ret != nil {
+		return ret
+	}
+
+	if ret = command.Commit(); ret != nil {
+		return ret
+	}
 
 	return scyna.OK
 }
