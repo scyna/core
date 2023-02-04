acc86637a8f997a9590024a0c83abe0552c73782
NGUYEN Hong San <hongsan@gmail.com>
2023-02-04 18:28:46 +0700

    refactoring

--- proto/compile.sh
+++ proto/compile.sh
@@ -1,5 +1,5 @@
 protoc -I=. --go_out=generated scyna.proto
 protoc -I=. --go_out=generated error.proto
-protoc -I=. --go_out=generated scheduler.proto
+protoc -I=. --go_out=generated task.proto
 protoc -I=. --go_out=generated engine.proto
 
