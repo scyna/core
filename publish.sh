9c646298ad4a66c7f5f7f17d04d679a2287dd586
KhanhND <khanhnd162@gmail.com>
2023-03-07 15:33:54 +0700

    update script publish

--- publish.sh
+++ publish.sh
@@ -1,2 +1,14 @@
-git tag $1
-git push origin $1
+#!/bin/bash
+
+read -p "Enter tag name: " tag
+read -p "Enter comment: " comment
+
+echo "Creating tag $tag with comment \"$comment\"..."
+
+git tag -a "$tag" -m "$comment"
+
+echo "Pushing tag $tag to remote repository..."
+
+git push origin "$tag"
+
+echo "Tag $tag created and pushed successfully!"
